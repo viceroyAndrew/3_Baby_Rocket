bda72cd1e5172050245a85a2d284af4d0168fd63
LAPTOP-3ENNLL7A\Lenovo <andrewbuuface@gmail.com>
2018-04-07 19:05:55 +0800

    refined farting noises

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -9,16 +9,20 @@ public class rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioSource; // initialising audio..
-    AudioClip fartThruster;
+    [SerializeField] AudioClip[] fartThrusters;
 
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>(); // links audioSource variable to gameObject
     }
-    
 
-    
+    private void PlayRandomSound()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, fartThrusters.Length);
+        audioSource.clip = fartThrusters[randomIndex];
+        audioSource.Play();
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,16 +31,18 @@ public class rocket : MonoBehaviour {
 	}
 
     private void ProcessInput()
-    {
-        
-
+    {       
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
             if (audioSource.isPlaying == false)
             {
-                audioSource.Play();
+                PlayRandomSound();
             }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
