a2b6f3a24236aaae51fae602b04f29d304fe8f7e
LAPTOP-3ENNLL7A\Lenovo <andrewbuuface@gmail.com>
2018-04-08 16:39:26 +0800

    rocket prefab updates

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -10,6 +10,8 @@ public class rocket : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource audioSource; // initialising audio..
     [SerializeField] AudioClip[] fartThrusters;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 50f;
 
     // Use this for initialization
     void Start () {
@@ -31,10 +33,35 @@ public class rocket : MonoBehaviour {
 	}
 
     private void ProcessInput()
-    {       
-        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+    {
+        Rotate();
+        Thrust();    
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) //take manual control of rotation
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false; //resume physics control
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (audioSource.isPlaying == false)
             {
                 PlayRandomSound();
@@ -44,14 +71,5 @@ public class rocket : MonoBehaviour {
         {
             audioSource.Stop();
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * Time.deltaTime *10); //corisponds to left
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * Time.deltaTime *10); //opposite to left
-        }
-
     }
 }
